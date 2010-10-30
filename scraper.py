4164641b775b0aad157af9fac15d333d4fac7038
avances123 <avances123@gmail.com>
2010-10-31 00:55:38 +0200

    finished

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import feedparser
+from BeautifulSoup import BeautifulSoup
+import re
+
 
 # GET http://www.blogger.com/feeds/blogID/posts/default?max-results=26
 
@@ -6,5 +9,11 @@ import feedparser
 d = feedparser.parse('http://www.blogger.com/feeds/8625828710657758692/posts/default?max-results=1')
 
 for entry in d.entries:
-    print entry.content
+    html = entry.content[0].value
+    soup = BeautifulSoup(html)
+    #print soup.prettify()
+    enlaces = soup.findAll(href=re.compile("megaupload|rapidshare"))
+    for enlace in enlaces:
+    	print enlace['href']
+
 
